7b2e9d9473e82065545edbf66e4a35b761c42c78
Bhargav Akula <mail.bhargavakula@gmail.com>
2025-05-02 17:47:09 -0400

    updating lsp.lua

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -25,6 +25,7 @@ require("mason-lspconfig").setup_handlers({
 
 -- Autocomplete:
 local cmp = require('cmp')
+local lsp = require('lsp-zero')
 cmp.setup({
   sources = {
     {name = 'nvim_lsp'},
@@ -42,4 +43,21 @@ cmp.setup({
 })
 
 
+-- hot keys that will be used if part of LSP
+lsp.on_attach(function (client, bufnr)
+	print("help")
+   local opts = {buffer = bufnr, remap = false}
 
+   vim.keymap.set("n", "gd", function () vim.lsp.buf.definition() end, opts)
+   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+   vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+end)
+
+lsp.setup()
