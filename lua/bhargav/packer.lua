0abcd48b1d8d5373985bf3ce8b7318633a060b7f
Bhargav Akula <mail.bhargavakula@gmail.com>
2025-05-02 18:47:01 -0400

    adding python debugging but still need to test

--- lua/bhargav/packer.lua
+++ lua/bhargav/packer.lua
@@ -4,58 +4,65 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  -- telescope.lua (for finding files)
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    -- telescope.lua (for finding files)
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-  -- rose-pine.lua (colorscheme)
-  use {
-	  "rose-pine/neovim",
-	  name = "rose-pine",
-	  config = function()
-		  vim.cmd("colorscheme rose-pine")
+    -- rose-pine.lua (colorscheme)
+    use {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            vim.cmd("colorscheme rose-pine")
 
-	  end
-  }
+        end
+    }
 
-  -- nvim v0.7.2
-  use({
-	  "kdheepak/lazygit.nvim",
-	  -- optional for floating window border decoration
-	  requires = {
-		  "nvim-lua/plenary.nvim",
-	  },
-  })
+    -- nvim v0.7.2
+    use({
+        "kdheepak/lazygit.nvim",
+        -- optional for floating window border decoration
+        requires = {
+            "nvim-lua/plenary.nvim",
+        },
+    })
 
-  use {
-	  'goolord/alpha-nvim',
-	  requires = { 'nvim-tree/nvim-web-devicons' },
-	  config = function ()
-		  require'alpha'.setup(require'alpha.themes.dashboard'.config)
-	  end
-  }
+    use {
+        'goolord/alpha-nvim',
+        requires = { 'nvim-tree/nvim-web-devicons' },
+        config = function ()
+            require'alpha'.setup(require'alpha.themes.dashboard'.config)
+        end
+    }
 
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('nvim-treesitter/playground', {run = ':TSUpdate'})
-  use('ThePrimeagen/harpoon')
-  use('mbbill/undotree')
-  use('tpope/vim-fugitive')
-  use('VonHeikemen/lsp-zero.nvim')
-  use('neovim/nvim-lspconfig')
-  use('hrsh7th/nvim-cmp')
-  use('hrsh7th/cmp-nvim-lsp')
-  use('hrsh7th/cmp-buffer')
-  use('hrsh7th/cmp-path')
-  use('hrsh7th/cmp-nvim-lua')
-  use('saadparwaiz1/cmp_luasnip')
-  use('L3MON4D3/LuaSnip')
-  use('rafamadriz/friendly-snippets')
-  use('williamboman/mason.nvim')
-  use('williamboman/mason-lspconfig.nvim')
+    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use('nvim-treesitter/playground', {run = ':TSUpdate'})
+    use('ThePrimeagen/harpoon')
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
+    use('VonHeikemen/lsp-zero.nvim')
+    use('neovim/nvim-lspconfig')
+    use('hrsh7th/nvim-cmp')
+    use('hrsh7th/cmp-nvim-lsp')
+    use('hrsh7th/cmp-buffer')
+    use('hrsh7th/cmp-path')
+    use('hrsh7th/cmp-nvim-lua')
+    use('saadparwaiz1/cmp_luasnip')
+    use('L3MON4D3/LuaSnip')
+    use('rafamadriz/friendly-snippets')
+    use('williamboman/mason.nvim')
+    use('williamboman/mason-lspconfig.nvim')
+    -- for debugging purpose
+    use("mfussenegger/nvim-dap")
+	use("nvim-neotest/nvim-nio")
+    use("rcarriga/nvim-dap-ui")
+    use("mfussenegger/nvim-dap-python")
+    use("theHamsta/nvim-dap-virtual-text")
+
 end)
