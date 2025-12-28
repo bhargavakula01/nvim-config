78b94dae92651ba0646a8390bd80c36132605848
Bhargav Akula <bhargavakula@Mac.attlocal.net>
2025-12-27 18:11:36 -0600

    test

--- lua/bhargav/packer.lua
+++ lua/bhargav/packer.lua
@@ -13,7 +13,7 @@ return require('packer').startup(function(use)
         requires = { {'nvim-lua/plenary.nvim'} }
     }
 
-    -- rose-pine.lua (colorscheme)
+    -- rose-pine.lua (colorscheme) asdasds
     -- use {
     --     "rose-pine/neovim",
     --     name = "rose-pine",
